77219ce747f869c74631cb4fc09fc7218cd1ed83
XxCristopherxXC <cr.barrueto@duocuc.cl>
2025-11-04 21:43:53 -0300

    Add DataStore Preferences and Coroutines dependencies
    Added dependencies for DataStore Preferences and Coroutines.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,13 @@ dependencies {
     // Navigation Compose
     implementation("androidx.navigation:navigation-compose:2.7.5")
 
+    // DataStore Preferences
+    implementation("androidx.datastore:datastore-preferences:1.1.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
     // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
